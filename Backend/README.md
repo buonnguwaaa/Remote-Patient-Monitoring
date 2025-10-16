bdad9707c1600af4f3b349b92401abfb4ef170c0
buonnguwaaa <nhannguyen04.it@gmail.com>
2025-10-16 22:12:51 +0700

    chore/nojira-000-setup-dependencies

---
+++ Backend/README.md
@@ -0,0 +1,7 @@
+# Install dependencies
+
+- Run _go mod tidy_
+
+# Start server
+
+- From ./Backend run _go run cmd/server/main.go_
--- Backend/cmd/server/main.go
+++ Backend/cmd/server/main.go
@@ -1,13 +1,37 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"github.com/sirupsen/logrus"
+	"github.com/swaggo/files"
+	"github.com/swaggo/gin-swagger"
+	"os"
+)
 
 func main() {
-	router := gin.Default()
-	router.GET("/ping", func(c *gin.Context) {
+	err := godotenv.Load()
+	if err != nil {
+		logrus.Warn("No .env file found")
+	}
+	logrus.Info("Successfully loaded .env file")
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	if os.Getenv("GIN_MODE") == "release" {
+		gin.SetMode(gin.ReleaseMode)
+	}
+
+	r := gin.Default()
+	r.Use(cors.Default())
+	r.GET("/health", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "pong",
+			"message": "OK",
 		})
 	})
-	router.Run() // listens on 0.0.0.0:8080 by default
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
+	r.Run(":" + port)
 }
