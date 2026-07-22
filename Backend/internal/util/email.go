53f6adf723f7646e9709beead3cf11f9a3a92951
Nguyen Huu Khanh Nhan <nhannhk@hasaki.vn>
2026-07-22 18:23:16 +0700

    [NOJIRA-000]: Fix slow API resp for admin registering patient path

--- Backend/internal/util/email.go
+++ Backend/internal/util/email.go
@@ -1,9 +1,17 @@
 package util
 
 import (
+	"crypto/tls"
 	"fmt"
+	"net"
 	"net/smtp"
 	"os"
+	"time"
+)
+
+const (
+	smtpDialTimeout  = 10 * time.Second
+	smtpTotalTimeout = 30 * time.Second
 )
 
 func SendEmail(to, subject, body string) error {
@@ -19,5 +27,51 @@ func SendEmail(to, subject, body string) error {
 		"Content-Type: text/html; charset=\"UTF-8\"\r\n\r\n"+
 		"%s",
 		from, to, subject, body)
-	return smtp.SendMail(addr, smtp.PlainAuth("", from, pass, host), from, []string{to}, []byte(msg))
+
+	// smtp.SendMail dials without any timeout, which lets a slow or
+	// unreachable SMTP server hang the caller indefinitely. Dial and bound
+	// the whole exchange ourselves instead.
+	conn, err := net.DialTimeout("tcp", addr, smtpDialTimeout)
+	if err != nil {
+		return err
+	}
+	if err := conn.SetDeadline(time.Now().Add(smtpTotalTimeout)); err != nil {
+		conn.Close()
+		return err
+	}
+
+	client, err := smtp.NewClient(conn, host)
+	if err != nil {
+		conn.Close()
+		return err
+	}
+	defer client.Close()
+
+	if ok, _ := client.Extension("STARTTLS"); ok {
+		if err := client.StartTLS(&tls.Config{ServerName: host}); err != nil {
+			return err
+		}
+	}
+	if ok, _ := client.Extension("AUTH"); ok {
+		if err := client.Auth(smtp.PlainAuth("", from, pass, host)); err != nil {
+			return err
+		}
+	}
+	if err := client.Mail(from); err != nil {
+		return err
+	}
+	if err := client.Rcpt(to); err != nil {
+		return err
+	}
+	w, err := client.Data()
+	if err != nil {
+		return err
+	}
+	if _, err := w.Write([]byte(msg)); err != nil {
+		return err
+	}
+	if err := w.Close(); err != nil {
+		return err
+	}
+	return client.Quit()
 }
